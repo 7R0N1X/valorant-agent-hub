69361566a6f74a60a44522e5dd0c2217abec00a7
7R0N1X <techsoftec.tronix@gmail.com>
2024-07-04 11:09:21 -0500

    feat(agents): data fetching in spanish

--- src/api/agents.js
+++ src/api/agents.js
@@ -5,7 +5,8 @@ let agents = []
 const fetchAgents = axios({
   url: "https://valorant-api.com/v1/agents",
   params: {
-    isPlayableCharacter: true
+    isPlayableCharacter: true,
+    language: "es-ES",
   }
 }).then((response) => {
   return agents = response.data.data.map((agent) => agent)
