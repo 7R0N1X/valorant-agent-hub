ce5183803193a09945c6fa07f1016ad5869313ae
7R0N1X <techsoftec.tronix@gmail.com>
2024-09-03 11:49:22 -0500

    feat: add new navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   adapter: vercel(),
   integrations: [tailwind()]
 });
