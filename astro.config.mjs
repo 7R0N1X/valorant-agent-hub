3dc41b959037df8f966ac7ad47dd4eed7ad04b12
7R0N1X <techsoftec.tronix@gmail.com>
2025-03-23 18:31:05 -0500

    chore: upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "static",
   adapter: vercel(),
   integrations: [tailwind()]
 });
