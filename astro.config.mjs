924dd8d51f0af8f61f74a92bf46598cc7ff94e26
7R0N1X <techsoftec.tronix@gmail.com>
2025-03-25 12:45:47 -0500

    feat(config): enhance Vite configuration with cache control and asset inline limit

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,14 @@ export default defineConfig({
   },
   adapter: vercel(),
   integrations: [tailwind()],
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
+    server: {
+      headers: {
+        'Cache-Control': 'public, max-age=31536000, immutable'
+      }
+    }
+  }
 });
