d9bd60c947c43502fdccf33e5e87d0426fec04b0
7R0N1X <techsoftec.tronix@gmail.com>
2025-03-25 09:38:21 -0500

    fix(config): enable prefetching in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   output: "static",
   adapter: vercel(),
   integrations: [tailwind()]
